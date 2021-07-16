03c3550945533a31c2126ae670aff64fda11c2df
sathvik <sivasathvikchandu99@gmail.com>
2021-07-16 11:23:42 +0530

    programs

---
+++ leetcode/15_07_21.py
@@ -0,0 +1,2 @@
+for j in range(5,1,-1):
+    print(j)
--- python/doublylinkedlist.py
+++ python/doublylinkedlist.py
@@ -1,80 +1,90 @@
 class Node:
-    def __init__(self,data):
-         self.data=data
-         self.next=None
-         self.prev=None
-class dll:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
     def __init__(self):
-        self.head=None
-    
+        self.head = None
 
-    def append(self,data):
+    def append(self, data):
         if self.head is None:
-            nn=Node(data)
-            nn.prev=None
-            self.head=nn
-        
+            new_node = Node(data)
+            new_node.prev = None
+            self.head = new_node
         else:
-            nn=Node(data)
-            curr=self.head
-            while curr.next:
-                curr=curr.next
-            curr.next=nn
-            nn.prev=curr
-            nn.next=None
+            new_node = Node(data)
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+            new_node.prev = cur
+            new_node.next = None
 
-
-    def prepend(self,data):
+    def prepend(self, data):
         if self.head is None:
-            curr=Node(data)
-            curr.prev=None
-            self.head= curr
-        
+            new_node = Node(data)
+            new_node.prev = None 
+            self.head = new_node
         else:
-            curr=Node(data)
-            self.head.prev=curr
-            curr.next=self.head
-            curr.prev=None
-            curr=self.head
+            new_node = Node(data)
+            self.head.prev = new_node
+            new_node.next = self.head
+            self.head = new_node
+            new_node.prev = None
 
-
-    def length(self):
-        curr= self.head
-        le=0
-        while curr:
-            curr=curr.next
-            le=le+1
-        return le
-
-
-    def printl(self):
-        curr=self.head
-        while curr!=None:
-            print(curr.data)
-            curr=curr.next
+    def print_list(self):
+        cur = self.head
+        while cur:
+            print(cur.data)
+            cur = cur.next
     
-    def delete(self,pos):
+    def addafter(self,key,data):
+        cur=self.head
+        while cur:
+            if cur.next is None and cur.data==key:
+                self.append(data)
+                return
+            elif cur.data==key:
+                new_node= Node(data)
+                nxt=cur.next
+                cur.next=new_node
+                new_node.next=nxt
+                new_node.prev=cur
+                nxt.prev=new_node
+            cur=cur.next
     
-        
-        if(self.length()==0):               #should get corrected
-            print("the list is empty")
-        
-        elif pos==1:
-            curr=self.head
-            curr.next=curr
-            ne=curr.next
-            self.head.next=ne
-            ne.prev=None
-            
-            
+    def addbefore(self,key,data):
+        cur=self.head
+        while cur:
+            if cur.prev is None and cur.data==key:
+                self.prepend(data)
+                return
+            elif cur.data==key:
+                new_node=Node(data)
+                prev= cur.prev
+                prev.next= new_node
+                cur.prev= new_node
+                new_node.next= cur
+                new_node.prev= prev
 
-dl=dll()
 
-dl.append(2)
-dl.append(3)
-dl.append(4)
-dl.prepend(9)
-dl.printl()
+            cur=cur.next
 
 
 
+
+dllist = DoublyLinkedList()
+dllist.prepend(0)
+dllist.append(1)
+dllist.append(2)
+dllist.append(3)
+dllist.append(4)
+dllist.prepend(5)
+
+dllist.addafter(1,9)
+dllist.addbefore(4,10)
+
+dllist.print_list()
